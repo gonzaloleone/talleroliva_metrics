cd7445a4e15bddd658168f1fb0991c0c8d380fd1
Gonzalo <gonzaleone03@gmail.com>
2025-07-20 23:19:11 -0300

    arreglo de tipeo y archivo env

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const { sequelize } = require('./models');
-const visitaRoutes = require('./routes/visitasRoutes');
+const visitaRoutes = require('./routes/visitaRoutes');
 require('dotenv').config();
 
 const PORT = process.env.PORT || 3000;
